163f04c9b1e79492fc5de66e1f8a293ad41acfc1
Callum Macdonald <git@callum-macdonald.com>
2018-10-19 14:45:45 +0200

    Add v2 to site title in config.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   blogPostDir: "sample-posts", // The name of directory that contains your posts.
   blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
   blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
-  siteTitle: "Gatsby Casper Starter", // Site title.
+  siteTitle: "Gatsby v2 Casper Starter", // Site title.
   siteTitleAlt: "GatsbyJS Casper Theme Starter", // Alternative site title for SEO.
   siteLogo:
     "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
